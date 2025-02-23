341430f93b6b01344ef800b8e7e7b23e4a6b1fd2
Yasz Avellia <zakamaragames@gmail.com>
2025-02-23 23:21:10 +0700

    menambah responsive

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,90 +18,92 @@
     <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
 </head>
 
-{{-- NOTIFICATION ALERT  --}}
-@if(SESSION("success"))
-<div id="alert" class="flex justify-center duration-300 scale-50 fixed top-[20px] right-[50%] translate-x-[50%] z-[100]">
-    <div class="md:w-[360px] w-[82vw] bg-green-100 border border-slate-700 rounded-md shadow-[3px_3px_0px_0px_rgba(22,101,52,1)]">
-        <div class="p-3 flex items-center h-full justify-center">
-            <div class="text-center ">
-                <h1 class="md:text-xl text-md font-semibold tracking-wide mb-1">Notification</h1>
-                <p class="tracking-wide md:text-sm text-xs leading-[1.1]">{{ SESSION("success") }}</p>
-            </div>
-        </div>
-    </div>
-</div>
-@endif
-
-@error('*')
-<div id="alert" class="flex justify-center duration-300 scale-50 fixed top-[20px] right-[50%] translate-x-[50%] z-[100]">
-    <div class="md:w-[360px] w-[82vw] bg-red-100 border border-slate-700 rounded-md shadow-[3px_3px_0px_0px_rgba(185,28,28,1)]">
-        <div class="p-3 flex items-center h-full justify-center">
-            <div class="text-center">
-                <h1 class="md:text-xl text-md font-semibold tracking-wide mb-1">Notification</h1>
-                <p class="tracking-wide md:text-sm text-xs leading-[1.1]">{{ $errors->first() }}</p>
-            </div>
-        </div>
-    </div>
-</div>
-@enderror
-
-
-
-
-{{-- MODAL LOGIN NAME --}}
-@if(!Auth::user())
-<div class="modal-auth w-full h-full bg-slate-500/20 fixed z-[99] top-0 right-0 bottom-0 left-0 flex justify-center items-center">
-    <div class="md:w-[450px] w-[90vw] flex-shrink-0 bg-white rounded-lg border-2 shadow-[3px_3px_0px_0px_rgba(0,0,0,1)] relative">
-        <img src="https://i.ibb.co.com/4ZzMmTj3/1740284891070.png" alt="MIKA" class="absolute top-[-140px] z-[-1] right-50 translate-x-[50%] w-[200px]" loading="lazy">
-        <div class="md:p-6 p-2 m-3">
-            <div class="text-center">
-                <h1 class="text-2xl font-semibold">Siapa namamu sensei ?</h1>
-                <form action="/login" method="POST" class="flex items-center mt-8 gap-3"> 
-                    @csrf
-                    <input type="text" name="name" autocomplete="off" required placeholder="Masukkan nama" maxlength="25" class="w-full shadow-[2px_2px_0px_0px_rgba(0,0,0,1)] border-2 py-3 px-4 text-sm text-slate-800 rounded-lg focus:outline-none  ">
-                    <button class="h-full cursor-pointer bg-white border p-3 rounded-md shadow-[3px_3px_0px_0px_rgba(0,0,0,1)]">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-send"><path d="M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z"/><path d="m21.854 2.147-10.94 10.939"/></svg>
-                    </button>
-                </form>
-
-            </div>
-        </div>
-    </div>
-</div>
-@endif
-
-
-{{-- MODAL DIALOG CONFIRM DELETE AND COMPLETED ACTIVITY  --}}
-<div id="modal-confirm" class="modal-confirm w-full h-full bg-slate-500/20 fixed z-[99] top-0 right-0 bottom-0 left-0 hidden justify-center items-center">
-    <div class="md:w-[450px] w-[90vw] flex-shrink-0 bg-white rounded-lg border-2 shadow-[3px_3px_0px_0px_rgba(0,0,0,1)] relative">
-        <img src="https://i.ibb.co.com/4ZzMmTj3/1740284891070.png" alt="MIKA" class="absolute top-[-140px] z-[-1] right-50 translate-x-[50%] w-[200px]" loading="lazy">
-        <div class="md:p-6 p-2 m-3">
-            <div class="text-center">
-                <h1 class="text-2xl font-semibold">Apakah kamu yakin?</h1>
-                <form action="" method="POST" id="confirmForm" class="flex items-center mt-8 gap-4 justify-center"> 
-                    @csrf
-                    <input type="hidden" name="_method" id="formMethod" value="">
-                    <div id="btn_cancel" class="font-semibold tracking-wide cursor-pointer bg-white border p-3 rounded-md shadow-[3px_3px_0px_0px_rgba(0,0,0,1)]">
-                        Cancel
-                    </div>
-                    <button id="btn_complete" class="font-semibold hidden tracking-wide cursor-pointer bg-green-400 border p-3 rounded-md shadow-[3px_3px_0px_0px_rgba(0,0,0,1)]">
-                        Selesai
-                    </button>
-                    <button id="btn_delete" class="font-semibold hidden tracking-wide cursor-pointer bg-red-400 border p-3 rounded-md shadow-[3px_3px_0px_0px_rgba(0,0,0,1)]">
-                        Hapus
-                    </button>
-
-                </form>
-
-            </div>
-        </div>
-    </div>
-</div>
 
 <body class="">
+    {{-- NOTIFICATION ALERT  --}}
+    @if(SESSION("success"))
+    <div id="alert" class="flex justify-center duration-300 scale-50 fixed top-[20px] right-[50%] translate-x-[50%] z-[100]">
+        <div class="md:w-[360px] w-[82vw] bg-green-100 border border-slate-700 rounded-md shadow-[3px_3px_0px_0px_rgba(22,101,52,1)]">
+            <div class="p-3 flex items-center h-full justify-center">
+                <div class="text-center ">
+                    <h1 class="md:text-xl text-md font-semibold tracking-wide mb-1">Notification</h1>
+                    <p class="tracking-wide md:text-sm text-xs leading-[1.1]">{{ SESSION("success") }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endif
+
+    @error('*')
+    <div id="alert" class="flex justify-center duration-300 scale-50 fixed top-[20px] right-[50%] translate-x-[50%] z-[100]">
+        <div class="md:w-[360px] w-[82vw] bg-red-100 border border-slate-700 rounded-md shadow-[3px_3px_0px_0px_rgba(185,28,28,1)]">
+            <div class="p-3 flex items-center h-full justify-center">
+                <div class="text-center">
+                    <h1 class="md:text-xl text-md font-semibold tracking-wide mb-1">Notification</h1>
+                    <p class="tracking-wide md:text-sm text-xs leading-[1.1]">{{ $errors->first() }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    @enderror
+
+
+
+
+    {{-- MODAL LOGIN NAME --}}
+    @if(!Auth::user())
+    <div class="modal-auth w-full h-full bg-slate-500/20 fixed z-[99] top-0 right-0 bottom-0 left-0 flex justify-center items-center">
+        <div class="md:w-[450px] w-[90vw] flex-shrink-0 bg-white rounded-lg border-2 shadow-[3px_3px_0px_0px_rgba(0,0,0,1)] relative">
+            <img src="https://i.ibb.co.com/4ZzMmTj3/1740284891070.png" alt="MIKA" class="absolute top-[-140px] z-[-1] right-50 translate-x-[50%] w-[200px]" loading="lazy">
+            <div class="md:p-6 p-2 m-3">
+                <div class="text-center">
+                    <h1 class="text-2xl font-semibold">Siapa namamu sensei ?</h1>
+                    <form action="/login" method="POST" class="flex items-center mt-8 gap-3"> 
+                        @csrf
+                        <input type="text" name="name" autocomplete="off" required placeholder="Masukkan nama" maxlength="25" class="w-full shadow-[2px_2px_0px_0px_rgba(0,0,0,1)] border-2 py-3 px-4 text-sm text-slate-800 rounded-lg focus:outline-none  ">
+                        <button class="h-full cursor-pointer bg-white border p-3 rounded-md shadow-[3px_3px_0px_0px_rgba(0,0,0,1)]">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-send"><path d="M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z"/><path d="m21.854 2.147-10.94 10.939"/></svg>
+                        </button>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    @endif
+
+
+    {{-- MODAL DIALOG CONFIRM DELETE AND COMPLETED ACTIVITY  --}}
+    <div id="modal-confirm" class="modal-confirm w-full h-full bg-slate-500/20 fixed z-[99] top-0 right-0 bottom-0 left-0 hidden justify-center items-center">
+        <div class="md:w-[450px] w-[90vw] flex-shrink-0 bg-white rounded-lg border-2 shadow-[3px_3px_0px_0px_rgba(0,0,0,1)] relative">
+            <img src="https://i.ibb.co.com/4ZzMmTj3/1740284891070.png" alt="MIKA" class="absolute top-[-140px] z-[-1] right-50 translate-x-[50%] w-[200px]" loading="lazy">
+            <div class="md:p-6 p-2 m-3">
+                <div class="text-center">
+                    <h1 class="text-2xl font-semibold">Apakah kamu yakin?</h1>
+                    <form action="" method="POST" id="confirmForm" class="flex items-center mt-8 gap-4 justify-center"> 
+                        @csrf
+                        <input type="hidden" name="_method" id="formMethod" value="">
+                        <div id="btn_cancel" class="font-semibold tracking-wide cursor-pointer bg-white border p-3 rounded-md shadow-[3px_3px_0px_0px_rgba(0,0,0,1)]">
+                            Cancel
+                        </div>
+                        <button id="btn_complete" class="font-semibold hidden tracking-wide cursor-pointer bg-green-400 border p-3 rounded-md shadow-[3px_3px_0px_0px_rgba(0,0,0,1)]">
+                            Selesai
+                        </button>
+                        <button id="btn_delete" class="font-semibold hidden tracking-wide cursor-pointer bg-red-400 border p-3 rounded-md shadow-[3px_3px_0px_0px_rgba(0,0,0,1)]">
+                            Hapus
+                        </button>
+
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
     <div class="max-w-5xl mx-auto w-full">
         <div class="my-12 flex justify-center w-full">  
-            <div class="md:w-[700px] w-full min-h-[700px] rounded-lg bg-white border-2 shadow-[6px_6px_0px_0px_rgba(0,0,0,1)]">
+            <div class="md:w-[700px] w-[92%] min-h-[700px] rounded-lg bg-white border-2 md:shadow-[6px_6px_0px_0px_rgba(0,0,0,1)] shadow-none">
                 <div class="header h-[50px] border-b-2">
                     <div class="p-5 flex items-center h-full gap-3">
                         <div class="w-[13px] h-[13px] rounded-full bg-red-400"></div>
@@ -110,13 +112,13 @@
                     </div>
                 </div>
                 <div class="p-5">
-                    <div class="flex justify-center items-center gap-9 mt-7">
+                    <div class="flex justify-center items-center md:gap-9 gap-5 mt-7">
                        
                         <a @if ($previousData != "" && $previousData->isNotEmpty()) href="?date={{$previousData->first()->created_at->format("d-m-Y")}}" @else class="invisible" @endif>
                             <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-left"><path d="m15 18-6-6 6-6"/></svg>
                         </a>
                         
-                        <div class="text-center w-[200px] ">
+                        <div class="text-center md:w-[200px] w-full">
                             @if (request()->query("date"))
                                 <h1 class="text-3xl tracking-wider font-bold">{{ $activities->first()->created_at->format("l") }}</h1>
                                 <p class="text-slate-600 text-sm mt-1">{{ $activities->first()->created_at->format("d M Y") }}</p>
@@ -144,7 +146,7 @@
                         <div class="w-[10px] h-[5px] bg-slate-900 rounded-full"></div>
                     </div>
                     <div class="mt-10">
-                        <form method="POST" action="/activity" class="mx-10 flex items-center h-full gap-3"> 
+                        <form method="POST" action="/activity" class="md:mx-10 mx-auto flex items-center h-full gap-3"> 
                             @csrf
                             <input type="text" name="name" required autocomplete="off" maxlength="250" placeholder="Lagi mau ngapain nih ? .." class="w-full shadow-[2px_2px_0px_0px_rgba(0,0,0,1)] border-2 py-3 px-4 text-sm text-slate-800 rounded-lg focus:outline-none  ">
                             <button class="h-full cursor-pointer bg-white border p-3 rounded-md shadow-[3px_3px_0px_0px_rgba(0,0,0,1)]">
@@ -152,7 +154,7 @@
                             </button>
                         </form>
                     </div>
-                    <div class="list mt-14 px-10">
+                    <div class="list mt-14 md:px-10 px-auto">
 
                         @if (Auth::user() && $activities->isNotEmpty() )
                             @foreach($activities as $activity)
